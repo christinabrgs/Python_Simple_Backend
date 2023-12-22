af8ae2010cbae0f482d0b3fa48fa1f40865e5499
Christina <christina.brgs@gmail.com>
2023-12-22 12:50:56 -0500

    python backend

---
+++ backend/routes/error.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, jsonify
+from werkzeug.exceptions import NotFound
+
+
+error_bp = Blueprint("errors", __name__)
+
+@error_bp.app_errorhandler(Exception)
+def handle_generic_exception(err):
+    return jsonify({"message": "Unknown error. Please chceck the logs for more details"}), 500
+
+@error_bp.app_errorhandler(NotFound)
+def handle_not_found(err):
+    return jsonify({"message": "this resource isn't available"}), 404
